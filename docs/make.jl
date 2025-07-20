3d1ef4472257095fb4cb92f1ca3bf46dd49e1988
Rino Beeli <40661605+rbeeli@users.noreply.github.com>
2025-07-20 04:07:53 +0200

    publish docs to gh-pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,12 @@
-push!(LOAD_PATH,"../src/")
+push!(LOAD_PATH, "../src/")
 
 using Documenter
 using RiskPerf
 
-makedocs(
-    sitename = "RiskPerf.jl",
+makedocs(;
+    sitename="RiskPerf.jl",
     # format = Documenter.HTML(prettyurls=false),
     # modules = [RiskPerf]
 )
+
+deploydocs(; repo="github.com/rbeeli/RiskPerf.jl.git")
